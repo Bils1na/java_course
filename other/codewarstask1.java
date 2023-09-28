b4654db50f557790044f3e88abfc755b819a939f
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-09-28 07:45:47 +0000

    cw task1

---
+++ other/codewarstask1.java
@@ -0,0 +1,15 @@
+package other;
+
+public class codewarstask1 {
+    public static void main(String[] args) {
+        String answer = convert(false);
+        System.out.println(answer);
+    }
+
+
+
+public static String convert(boolean b) {
+    String result = String.valueOf(b);
+    return result;
+}
+}
