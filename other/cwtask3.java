bc24098b9dd535a39332f861b7468d41af178820
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-09-28 08:46:55 +0000

    cw task3 fix

--- other/cwtask3.java
+++ other/cwtask3.java
@@ -4,19 +4,28 @@ import java.util.*;
 public class cwtask3 {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{5, 10, 2, 5, 23, 11};
+        int[] nums = new int[]{5, 10, 2, 10};
         System.out.println(sum(nums));
     }
 
 	public static int sum(int[] numbers) {
-		int max = Arrays.stream(numbers).max().getAsInt();
-		int min = Arrays.stream(numbers).min().getAsInt();
-		int sum = 0;
-		
-		for (int item: numbers) {
-            sum += item == max || item == min ? 0 : item; 
+        int sum = 0; 
+        if (numbers != null && numbers.length != 0) 
+        {
+          int max = Arrays.stream(numbers).max().getAsInt();
+          int min = Arrays.stream(numbers).min().getAsInt();
+          int count = 0;
+        
+          for (int item: numbers) 
+          { 
+            if (item == min || item == max && count != 2) 
+            {
+              count += 1;
+            } else {
+              sum += item;
+            }
+          }
         }
-
         return sum;
-	}
+    }
 }
