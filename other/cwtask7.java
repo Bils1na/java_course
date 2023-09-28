ab7d8da92378413701bcbca413665f0130772a19
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-09-28 11:20:26 +0000

    fix task7

--- other/cwtask7.java
+++ other/cwtask7.java
@@ -7,6 +7,6 @@ public class cwtask7 {
     }
 
     public static boolean isLove(final int flower1, final int flower2) {
-        return flower1 % 2 == 0 && flower2 % 2 == 0 || flower1 == flower2 ? false : true;
+        return (flower1 % 2 == 0 && flower2 % 2 == 0) || (flower1 % 2 != 0 && flower2 % 2 != 0) ? false : true;
     }
 }
