afebed954e65fc4d2936200e6c98e1e0d027844e
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-09-28 10:52:00 +0000

    added task7

---
+++ other/cwtask7.java
@@ -0,0 +1,12 @@
+package other;
+
+public class cwtask7 {
+
+    public static void main(String[] args) {
+        System.out.println(isLove(2, 3));
+    }
+
+    public static boolean isLove(final int flower1, final int flower2) {
+        return flower1 % 2 == 0 && flower2 % 2 == 0 || flower1 == flower2 ? false : true;
+    }
+}
