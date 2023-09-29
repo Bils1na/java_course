e3d40274650fa9745a96b8d5501a055e9765f96f
Artemiy Pilipchuk <100224557+Bils1na@users.noreply.github.com>
2023-09-29 09:39:17 +0000

    cwtask9 added

--- other/cwtask9.java
+++ other/cwtask9.java
@@ -1,5 +1,18 @@
 package other;
 
 public class cwtask9 {
-    
+    public static void main(String[] args) {
+        String[] strings = new String[]{"3 years old", "1 year old", "9 years old"};
+        for (String string : strings) {
+            System.out.println(howOld(string));
+        }
+
+    }
+
+    public static int howOld(final String herOld) {
+        for (int i = 0; i < herOld.length(); i++) {
+          return herOld.charAt(i) >= '0' && herOld.charAt(i) <= '9' ? Character.getNumericValue(herOld.charAt(i)) : -1;
+        }
+        return -1;
+      }
 }
