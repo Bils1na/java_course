69952c2a34468704479445dd7ddc4008af15e809
bilsina <bils1n@icloud.com>
2023-10-22 18:24:13 +0300

    added lec4

---
+++ lecture4/main.java
@@ -0,0 +1,70 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class main {
+    public static void main(String[] args) {
+        // LinkedList<Integer> ll = new LinkedList<Integer>();
+        // ll.add(1);
+        // ll.add(2);
+        // ll.add(3);
+
+        // Queue<Integer> queue = new LinkedList<Integer>();
+        // queue.add(1);
+        // queue.add(2);
+        // queue.add(3);
+
+        // int item = queue.remove();
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        // pq.add(123);
+        // pq.add(3);
+        // pq.add(13);
+        // pq.add(1);
+        // System.out.println(pq);
+        // System.out.println(pq.poll());
+        // System.out.println(pq.poll());
+        // System.out.println(pq.poll());
+        // System.out.println(pq.poll());
+        // System.out.println(pq.poll());
+
+        // Deque<Integer> deque = new ArrayDeque<>();
+        // deque.addFirst(1);
+        // deque.addLast(2);
+        
+        // var exp = "1 2 3 * +".split(" ");
+        // int res = 0;
+        // System.out.println(exp);
+
+        // Stack<Integer> st = new Stack<>();
+        // for (int i = 0; i < exp.length; i++) {
+            
+        //     if (Character.isDigit(exp[i])) {
+        //         st.push(Integer.parseInt(exp[i]));
+        //     } else {
+        //         switch (exp[i]) {
+        //             case "+" :
+        //                 res = st.pop() + st.pop();
+        //                 st.push(res);
+        //                 break;
+        //             case "-" :
+        //                 res = st.pop() - st.pop();
+        //                 st.push(res);
+        //                 break;
+        //             case "*" :
+        //                 res = st.pop() * st.pop();
+        //                 st.push(res);
+        //                 break;
+        //             case "/" :
+        //                 res = st.pop() / st.pop();
+        //                 st.push(res);
+        //                 break;
+        //         }
+        //     }
+        // }
+        // System.out.printf("%d\n", st.pop());
+    }
+}
